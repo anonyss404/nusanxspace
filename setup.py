c9d824e021724c25350bc58febc5c5bbae077fb3
tinuxhere <anonyss404@gmail.com>
2026-02-16 22:36:23 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,41 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read().splitlines()
+
 setup(
     name="nusanxspace",
-    version="3.0",
-    packages=find_packages(),
-    install_requires=[
-        "requests",
-        "python-dotenv",
-        "colorama"
-    ],
+    version="3.0.0",
     author="Anonys",
-    description="Bug Bounty Toolkit",
+    author_email="anonys@example.com",
+    description="Advanced Bug Bounty Toolkit - Security Testing Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/username/NUSANXSPACE",
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Information Technology",
+        "Topic :: Security",
     ],
+    python_requires=">=3.7",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "nusanxspace=src.controller.bot:main",
+            "nusanxspace-builder=tools.builder:main",
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
 )
